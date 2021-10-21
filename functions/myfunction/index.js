18bb2fa9aed08dd404cb8ea1b99ef3b3b72c3034
Mike Burrnside <mburnside@mburnsi-ltmkrcu.internal.salesforce.com>
2021-10-21 14:25:11 +1100

    new logging

--- functions/myfunction/index.js
+++ functions/myfunction/index.js
@@ -187,10 +187,6 @@ function distance(latitudeSt, longitudeSt, latitudeSch, longitudeSch) {
     
     const doc = new PDFDocument;
     doc.fontSize(10).text(text, 50, 50);
-  doc
-  .font('fonts/PalatinoBold.ttf')
-  .fontSize(25)
-  .text('Some text with an embedded font!', 100, 100);
     doc.end();
 
     const data = await getStream.buffer(doc);
