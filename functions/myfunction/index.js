75ebd29ea309b3fdff192ea78b0dbbf579f21f7b
Mike Burrnside <mburnside@mburnsi-ltmkrcu.internal.salesforce.com>
2021-10-21 15:31:25 +1100

    test

--- functions/myfunction/index.js
+++ functions/myfunction/index.js
@@ -207,6 +207,27 @@ function distance(latitudeSt, longitudeSt, latitudeSch, longitudeSch) {
     
     const doc = new PDFDocument;
     doc.fontSize(10).text(text, 50, 50);
+    doc
+  .addPage()
+  .fontSize(25)
+  .text('Here is some vector graphics...', 100, 100);
+  doc
+  .save()
+  .moveTo(100, 150)
+  .lineTo(100, 250)
+  .lineTo(200, 250)
+  .fill('#FF3300');
+
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus.  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl.';
+doc.addPage();
+doc.fontSize(8);
+doc.text(`This text is left aligned. ${lorem}`, {
+  width: 410,
+  align: 'left'
+}
+);
+
+
     doc.end();
 
     const data = await getStream.buffer(doc);
