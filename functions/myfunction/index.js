34f5b2973553c6dd442360c93fb830669291e05d
Mike Burrnside <mburnside@mburnsi-ltmkrcu.internal.salesforce.com>
2021-10-29 13:59:58 +1100

    test

--- functions/myfunction/index.js
+++ functions/myfunction/index.js
@@ -207,8 +207,8 @@ async function createPdf(text) {
 const doc=new PDFDocument();
   doc
     .fontSize(20)
-    .text("Salesforce functions built some some vector graphics...", 100, 100);
-  doc.save().moveTo(100, 150).lineTo(100, 250).lineTo(200, 250).fill("#0000");
+    .text("I used a Salesforce function to build some some vector graphics...", 100, 100);
+  doc.save().moveTo(100, 150).lineTo(100, 250).lineTo(200, 250).fill("#00FF00");
   doc.fontSize(14).text(text, 50, 50);
   const lorem =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus.  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl.";
